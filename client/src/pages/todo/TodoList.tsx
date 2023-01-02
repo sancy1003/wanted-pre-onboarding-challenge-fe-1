a6fd252e70eca379d88cc784e09a2a0f520d4013
chanyoung <kipa1003@naver.com>
2023-01-02 17:03:15 +0900

    add: todo add ui component

--- client/src/pages/todo/TodoList.tsx
+++ client/src/pages/todo/TodoList.tsx
@@ -38,7 +38,30 @@ const TodoList = () => {
 						))}
 					</ul>
 				</div>
-				<div>detail</div>
+				<div className="flex flex-col w-[500px] bg-white rounded-md p-5">
+					<div className="mb-5">
+						<h1 className="font-bold text-lg">일정 추가</h1>
+					</div>
+					<div className="flex flex-col gap-y-4 h-full">
+						<input
+							type="text"
+							placeholder="일정 이름"
+							className="border border-slate-200 bg-slate-100 rounded-sm px-4 py-3"
+						/>
+						<textarea
+							placeholder="일정 내용"
+							className="flex-1 border border-slate-200 bg-slate-100 rounded-sm px-4 py-3 resize-none"
+						/>
+						<div className="flex justify-end gap-x-4">
+							<button className="text-slate-600 hover:text-cyan-700 hover:font-bold">
+								등록
+							</button>
+							<button className="text-slate-600 hover:text-cyan-700 hover:font-bold">
+								취소
+							</button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
